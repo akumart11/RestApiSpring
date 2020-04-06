37770e1b6fee9d49fd362a2e09157ed3284b7a2c
Avinash <akumart11@gmail.com>
2020-04-06 19:08:01 +0530

    Added URI in properties file

--- src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.springrestclientexamples.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,14 +14,17 @@ public class ApiServiceImpl implements ApiService {
 	
 	private RestTemplate restTemplate;
 	
-	public ApiServiceImpl(RestTemplate restTemplate) {
+	public final String api_url;
+	
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
 		this.restTemplate = restTemplate;
+		this.api_url = api_url;
 	}
 
 	@Override
 	public List<User> getUser(Integer limit) {
 		
-		UserData userdata = restTemplate.getForObject("http://private-anon-872f6db5b2-apifaketory.apiary-mock.com/api/user?limit" + limit, UserData.class);
+		UserData userdata = restTemplate.getForObject( api_url + limit, UserData.class);
 		return userdata.getData();
 	}
 
