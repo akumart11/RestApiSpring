a206346a7e190e0266f7028562896d37052c8325
Avinash <akumart11@gmail.com>
2020-04-06 21:10:26 +0530

    Added Thymeleaf pages and controllers. Modified Services

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,10 +1,11 @@
-package guru.springframework.springrestclientexamples.service;
+package guru.springframework.springrestclientexamples.services;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
@@ -24,7 +25,9 @@ public class ApiServiceImpl implements ApiService {
 	@Override
 	public List<User> getUser(Integer limit) {
 		
-		UserData userdata = restTemplate.getForObject( api_url + limit, UserData.class);
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(api_url).queryParam("limit", 1);
+		
+		UserData userdata = restTemplate.getForObject( uriBuilder.toUriString(), UserData.class);
 		return userdata.getData();
 	}
 
--- src/test/java/guru/springframework/springrestclientexamples/services/ApiServiceImplTest.java
+++ src/test/java/guru/springframework/springrestclientexamples/services/ApiServiceImplTest.java
@@ -1,4 +1,4 @@
-package guru.springframework.springrestclientexamples.service;
+package guru.springframework.springrestclientexamples.services;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import guru.springframework.api.domain.User;
+import guru.springframework.springrestclientexamples.services.ApiService;
 
 
 @RunWith(SpringRunner.class)
